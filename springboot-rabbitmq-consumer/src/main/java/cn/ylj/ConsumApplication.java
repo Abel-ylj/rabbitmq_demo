e0a25845b6719a4c0238390a52a9992c2be6dcbf
ylj <aboutlistener@163.com>
2021-01-26 18:55:19 +0800

    springboot继承rabbitmq: 生产者工程&消费者工程搭建

---
+++ springboot-rabbitmq-consumer/src/main/java/cn/ylj/ConsumApplication.java
@@ -0,0 +1,11 @@
+package cn.ylj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumApplication.class, args);
+    }
+}
